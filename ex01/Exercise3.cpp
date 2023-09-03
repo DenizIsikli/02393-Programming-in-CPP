79ffb6ba1b6c9aab089438c664e58648d6a1ab8b
Bobby <s215818@dtu.dk>
2023-09-03 22:33:57 +0200

    Exercise 3

---
+++ ex01/Exercise3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+class TwoIntegers {
+public:
+    void twointegers(int x, int y) {
+        if(x == y) {
+            cout << "x is equal to y" << endl;
+        } else if(x > y) {
+            cout <<"x is bigger than y" << endl;
+        } else {
+            cout << "x is smaller than y" << endl;
+        }
+    }
+};
+
+
+int main() {
+    TwoIntegers twointegers;
+    int x;
+    int y;
+
+    cin >> x;
+    cin >> y;
+
+    twointegers.twointegers(x, y);
+    return 0;
+}
