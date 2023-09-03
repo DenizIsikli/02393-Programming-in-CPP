591752a19cff512b0f986be1a76b0201dba48262
Bobby <s215818@dtu.dk>
2023-09-04 01:42:17 +0200

    Exercise 3

---
+++ ex02/Exercise3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+class ApproximatingPi {
+    public:
+        void approximatingpi(unsigned int n) {
+            double pi = 0;
+            for (int i = 0; i < n; i++) {
+                pi += pow(-1, i) / (2 * i + 1);
+
+                cout << 4 * pi << endl;
+            }
+        }
+};
+
+
+int main() {
+    ApproximatingPi approximatingpi;
+    unsigned int n;
+
+    cin >> n;
+
+    approximatingpi.approximatingpi(n);
+    return 0;
+}
