25559fd6512c4af2ff3c59f864ffc6bd028fb22a
Bobby <s215818@dtu.dk>
2023-09-03 22:33:44 +0200

    Exercise 1

---
+++ ex01/Exercise1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class HelloWorld {
+public:
+    void helloworld() {
+        cout << "Hello World!" << endl;
+    }
+};
+
+
+int main() {
+    HelloWorld helloworld;
+
+    helloworld.helloworld();
+    return 0;
+}
