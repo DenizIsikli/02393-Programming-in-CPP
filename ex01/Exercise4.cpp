085745ff0a58f9248c1f3581c5070db18d54e426
Bobby <s215818@dtu.dk>
2023-09-03 22:34:01 +0200

    Exercise 4

---
+++ ex01/Exercise4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+class ThreeDouble {
+public:
+    void threedouble(double x, double y, double z) {
+        cout << (x+y)-z << endl;
+    }
+};
+
+
+int main() {
+    ThreeDouble threedouble;
+    double x;
+    double y;
+    double z;
+
+    cin >> x;
+    cin >> y;
+    cin >> z;
+
+    threedouble.threedouble(x, y, z);
+    return 0;
+}
