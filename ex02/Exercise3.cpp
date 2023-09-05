d6405620ced6135daa2c96949932e244af3175f5
Bobby <s215818@dtu.dk>
2023-09-06 01:05:32 +0200

    Fix

--- ex02/Exercise3.cpp
+++ ex02/Exercise3.cpp
@@ -7,11 +7,12 @@ class ApproximatingPi {
     public:
         void approximatingpi(unsigned int n) {
             double pi = 0;
+            double pi_result = 0;
+
             for (int i = 0; i < n; i++) {
                 pi += pow(-1, i) / (2 * i + 1);
-
-                cout << 4 * pi << endl;
             }
+            cout << 4 * pi << endl;
         }
 };
 
