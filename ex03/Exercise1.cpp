c57af24d3fd9f86ab5082d92d94fd295351d8d5b
Bobby <s215818@dtu.dk>
2023-09-06 00:50:53 +0200

    Fun with bags 1

--- ex03/Exercise1.cpp
+++ ex03/Exercise1.cpp
@@ -1,2 +1,46 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+#define MAX 1000
+
+class FunWithBags {
+    public:
+        void funwithbags() {
+            vector<int> numbers(MAX, 0);
+
+            while (true) {
+                string command;
+                cin >> command;
+
+                int n;
+                cin >> n;
+
+                if (n < 1 || n > 1000) {
+                    return;
+                } else {
+                    if (command == "add") {
+                        numbers[n]++;
+                    } else if (command == "del" && numbers[n] > 0) {
+                        numbers[n] = 0;
+                    } else if (command == "qry") {
+                        // check if n is in the bag
+                        if (numbers[n] > 0) {
+                            cout << "T";
+                        } else {
+                            cout << "F";
+                        }
+                    } else if (command == "quit") {
+                        return;
+                    }
+                }
+            }
+        } 
+};
+
+
+int main() {
+    FunWithBags funwithbags;
+    funwithbags.funwithbags();
+    return 0;
+}
