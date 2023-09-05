95946916b8a11fc6c3c53be9c184e53871cdc130
Bobby <s215818@dtu.dk>
2023-09-05 20:44:01 +0200

    Fix

--- ex01/Exercise3.cpp
+++ ex01/Exercise3.cpp
@@ -6,11 +6,11 @@ class TwoIntegers {
     public:
         void twointegers(int x, int y) {
             if(x == y) {
-                cout << "x is equal to y" << endl;
+                cout << x << " is equal to " << y << endl;
             } else if(x > y) {
-                cout <<"x is bigger than y" << endl;
+                cout << x << " is bigger than " << y << endl;
             } else {
-                cout << "x is smaller than y" << endl;
+                cout << x << " is smaller than " << y << endl;
             }
         }
 };
