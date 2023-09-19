48579910d6989ca03429f2ba0d62003c5db36923
Bobby <s215818@dtu.dk>
2023-09-19 16:44:27 +0200

    Exercise3.cpp done

--- ex03/Exercise3.cpp
+++ ex03/Exercise3.cpp
@@ -1,69 +1,70 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
-#define MAX 50
+using namespace std;
 
 struct Contact {
     string firstName;
     string middleName;
     string lastName;
-    int phoneNumber;
+    string phoneNumber;
     string streetAddress;
-    string houseAddress;
-    int zipCode;
+    string houseNumber;
+    string zipCode;
     string region;
 };
 
 class AddressBook {
 public:
+    vector<Contact> addressBook;
+
+    AddressBook() : addressBook(50) {}
+
     void addressbook() {
-        vector<Contact> contacts(MAX);
+        string command;
 
-        while (true) {
-            string command;
-            cin >> command;
-
+        while (cin >> command) {
             if (command == "add") {
                 int n;
-                cin >> n; 
+                cin >> n;
+                n--;
 
-                if (n >= 1 && n <= 50) {
-                    Contact contact;
-                    cin >> contact.firstName >> contact.middleName >> contact.lastName >> contact.phoneNumber
-                        >> contact.streetAddress >> contact.houseAddress >> contact.zipCode >> contact.region;
-                    contacts[n - 1] = contact;
-                } else {
-                    cout << "Invalid page number" << endl;
+                if (n >= 0 && n < 50 && addressBook[n].firstName.empty()) {
+                    Contact &contact = addressBook[n];
+                    cin >> contact.firstName >> contact.middleName >> contact.lastName
+                        >> contact.phoneNumber >> contact.streetAddress >> contact.houseNumber
+                        >> contact.zipCode >> contact.region;
+                }
+            } else if (command == "clr") {
+                int n;
+                cin >> n;
+                n--;
+
+                if (n >= 0 && n < 50) {
+                    addressBook[n] = Contact();
                 }
             } else if (command == "qry") {
                 int n;
                 cin >> n;
+                n--;
 
-                if (n >= 1 && n <= 50) {
-                    if (!contacts[n - 1].firstName.empty()) {
-                        cout << contacts[n - 1].firstName << ", "
-                            << contacts[n - 1].middleName << ", "
-                            << contacts[n - 1].lastName << ", "
-                            << contacts[n - 1].phoneNumber << ", "
-                            << contacts[n - 1].streetAddress << ", "
-                            << contacts[n - 1].houseAddress << ", "
-                            << contacts[n - 1].zipCode << ", "
-                            << contacts[n - 1].region << endl;
-                    } else {
-                        cout << "Contact does not exist" << endl;
+                if (n >= 0 && n < 50) {
+                    Contact &contact = addressBook[n];
+
+                    if (!contact.firstName.empty()) {
+                        cout << contact.firstName << "," << contact.middleName << ","
+                             << contact.lastName << "," << contact.phoneNumber << ","
+                             << contact.streetAddress << "," << contact.houseNumber << ","
+                             << contact.zipCode << "," << contact.region << endl;
                     }
-                } else {
-                    cout << "Invalid page number" << endl;
+                } else if (command == "quit") {
+                    break;
                 }
-            } else if (command == "quit") {
-                return;
-            } else {
-                cout << "Invalid command" << endl;
             }
         }
     }
 };
+
 
 int main () {
     AddressBook addressbook;
